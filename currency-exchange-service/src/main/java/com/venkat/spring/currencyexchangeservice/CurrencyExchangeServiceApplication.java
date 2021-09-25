6980d8b53f575a5de5ce4002db33fb10ea13ba60
VenkateshSubramaniyan <srvenkat16@gmail.com>
2021-09-25 18:11:22 +0530

    Added Sleuth Dependancy

--- currency-exchange-service/src/main/java/com/venkat/spring/currencyexchangeservice/CurrencyExchangeServiceApplication.java
+++ currency-exchange-service/src/main/java/com/venkat/spring/currencyexchangeservice/CurrencyExchangeServiceApplication.java
@@ -2,7 +2,10 @@ package com.venkat.spring.currencyexchangeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import brave.sampler.Sampler;
 
 @EnableJpaRepositories
 @SpringBootApplication
@@ -11,5 +14,11 @@ public class CurrencyExchangeServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CurrencyExchangeServiceApplication.class, args);
 	}
+	
+	@Bean
+	public Sampler defaultSampler() {
+		return Sampler.ALWAYS_SAMPLE;
+	}
+
 
 }
