a8d9ac8ef838396c7ead3729bf381135162f45b1
VenkateshSubramaniyan <srvenkat16@gmail.com>
2021-09-25 15:03:25 +0530

    Bind All Services to Zeul Gateway

---
+++ zuul-gateway/src/main/java/com/venki/spring/zuulgateway/ZuulLoggingFilter.java
@@ -0,0 +1,49 @@
+package com.venki.spring.zuulgateway;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.http.protocol.RequestContent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+
+@Component
+public class ZuulLoggingFilter extends ZuulFilter {
+
+	
+	private static final Logger log = LoggerFactory.getLogger(ZuulLoggingFilter.class);
+
+	@Override
+	public boolean shouldFilter() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public Object run() throws ZuulException {
+		HttpServletRequest request = RequestContext.getCurrentContext().getRequest();
+		log.info("request -> {} request uri-> {}",
+				request,request.getRequestURI());
+
+		
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String filterType() {
+		// TODO Auto-generated method stub
+		return "pre";
+	}
+
+	@Override
+	public int filterOrder() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
