eb4e6df3a4326d35fa414b3328273a7ee5228b7c
VenkateshSubramaniyan <srvenkat16@gmail.com>
2021-10-03 22:08:11 +0530

    Added Logging Filter which can log incoming calls

---
+++ API-GATEWAY/src/main/java/com/venki/spring/LoggingFilter.java
@@ -0,0 +1,30 @@
+package com.venki.spring;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import reactor.core.publisher.Mono;
+
+@Component
+public class LoggingFilter implements GlobalFilter {
+
+	
+	private static final Logger log = LoggerFactory.getLogger(LoggingFilter.class);
+
+	
+	@Override
+	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+		
+		log.info("Path of request Received -> {}", 
+				exchange.getRequest().getPath());
+
+		return chain.filter(exchange);
+	}
+
+	
+	
+}
